2cda8a7e868b949cd9f19c3e1fe7e10e3fc7b5e6
Siarhei <greysiarhei@gmail.com>
2023-05-11 19:15:22 +0300

    добавил код

--- src/homework1/Class1.java
+++ src/homework1/Class1.java
@@ -3,6 +3,7 @@ package homework1;
 public class Class1 {
     public static void main(String[] args) {
         System.out.println("homework");
-        int x;
+        int x=8;
+        System.out.println(x);
     }
 }
